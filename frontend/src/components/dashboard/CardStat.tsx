53f5cf37f0eb27bd18b8bb97a4c317ffabec4251
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2025-05-22 01:42:02 +0200

    refactor: simplify CardStat component structure and remove unused imports

--- frontend/src/components/dashboard/CardStat.tsx
+++ frontend/src/components/dashboard/CardStat.tsx
@@ -1,25 +1,24 @@
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
 
 export const CardStat: React.FC<{
-  title: string;
-  value: string;
-  icon: React.ReactNode;
-  className?: string;
+	title: string;
+	value: string;
+	icon: React.ReactNode;
+	className?: string;
 }> = ({ title, value, icon }) => (
+	<div className="flex flex-col sm:w-sm md:w-96 lg:w-1/4 ">
+		<Card className="flex items-start py-3 px-6">
+			<CardContent className="flex p-3 md:grid-cols-2 w-max gap-3">
+				<div className="flex p-3 items-center h-16 w-16 rounded-full bg-amber-100 text-amber-500">
+					{icon}
+				</div>
 
-  <div className="flex flex-col w-72 md:w-96 lg:w-1/4 ">
-    <Card className="flex items-start p-3">
-      <CardContent className="flex p-3 md:grid-cols-2 w-max">
-        <div className="flex p-3 items-center h-16 w-16 rounded-full bg-amber-100 text-amber-500">
-          {icon}
-        </div>
-
-        <div className="flex flex-col items-start p-3 md:p-3">
-          <div className="text-2xl font-bold">{value}</div>
-          <CardTitle className="text-sm font-normal">{title}</CardTitle>
-        </div>
-      </CardContent>
-    </Card>
-  </div>
+				<div className="flex flex-col items-start p-3 gap-2 flex-shrink-0 self-stretch">
+					<div className="flex text-xl font-bold">{value}</div>
+					<div className="flex text-sm font-normal">{title}</div>
+				</div>
+			</CardContent>
+		</Card>
+	</div>
 );
