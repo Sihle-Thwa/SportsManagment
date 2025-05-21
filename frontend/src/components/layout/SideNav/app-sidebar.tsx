c79b0bd90eb822004710ba956c4528c350f8675b
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2025-05-21 23:30:37 +0200

    fix: remove unnecessary grid class for improved layout in AppSideBar component

--- frontend/src/components/layout/SideNav/app-sidebar.tsx
+++ frontend/src/components/layout/SideNav/app-sidebar.tsx
@@ -1,11 +1,11 @@
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenuButton,
-  SidebarRail,
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenuButton,
+	SidebarRail,
 } from "@/components/ui/sidebar";
 import { NavMain } from "./nav-main";
 import { NavSecondary } from "./nav-secondary";
@@ -14,39 +14,40 @@ import { useState } from "react";
 import { House } from "lucide-react";
 
 interface AppSideBarProps {
-  currentPath: string;
+	currentPath: string;
 }
 
 export function AppSideBar({ currentPath }: AppSideBarProps) {
-  const [activeRoute, setActiveRoute] = useState(currentPath);
+	const [activeRoute, setActiveRoute] = useState(currentPath);
 
-  return (
-    <Sidebar collapsible="icon">
-      <SidebarHeader>
-        <SidebarMenuButton size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
-        <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-          <House className="h-4 w-4" />
-        </div>
-        <div className="grid flex-1 text-left text-sm leading-tight">
-          <span className="truncate font-semibold">U-Organise</span>
-        </div>
-        </SidebarMenuButton>
-        
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain activeRoute={activeRoute} setActiveRoute={setActiveRoute} />
+	return (
+		<Sidebar collapsible="icon">
+			<SidebarHeader>
+				<SidebarMenuButton
+					size="lg"
+					className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+				>
+					<div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+						<House className="h-4 w-4" />
+					</div>
+					<div className="flex-1 text-left text-sm leading-tight">
+						<span className="truncate font-semibold">U-Organise</span>
+					</div>
+				</SidebarMenuButton>
+			</SidebarHeader>
+			<SidebarContent>
+				<NavMain activeRoute={activeRoute} setActiveRoute={setActiveRoute} />
 
-        <SidebarGroupLabel>Settings</SidebarGroupLabel>
+				<SidebarGroupLabel>Settings</SidebarGroupLabel>
 
-        <NavSecondary />
-      </SidebarContent>
-      <SidebarFooter className="mt-auto">
-        {/* Fix Icon to display center when sidenav bar is 'closed' */}
-        <NavUser />
-      </SidebarFooter>
-      <SidebarRail />
-    </Sidebar>
-  );
+				<NavSecondary />
+			</SidebarContent>
+			<SidebarFooter className="mt-auto">
+				{/* Fix Icon to display center when sidenav bar is 'closed' */}
+				<NavUser />
+			</SidebarFooter>
+			<SidebarRail />
+		</Sidebar>
+	);
 }
 export default AppSideBar;
