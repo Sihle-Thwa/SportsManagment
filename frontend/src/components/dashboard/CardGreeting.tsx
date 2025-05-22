aea4ed030112c742a6d38688c6cfa37985090b71
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2025-05-22 02:28:33 +0200

    refactor: streamline CardGreeting component structure and remove unused imports

--- frontend/src/components/dashboard/CardGreeting.tsx
+++ frontend/src/components/dashboard/CardGreeting.tsx
@@ -1,41 +1,35 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 import greetingCardImg from "@/assets/images/standing-dashboard-image.svg";
 
 export function CardGreeting() {
-  return (
-    <div className="flex w-full h-full border rounded-lg overflow-hidden bg-white shadow-sm">
-      <Card className="overflow-hidden h-full w-full rounded-none  ">
-        <CardContent className="grid grid-cols-1 md:grid-cols-2 gap-0 ">
-          <div className="items-start justify-evenly p-6">
-            <CardHeader className="flex flex-col items-center text-center">
-              <CardTitle className="text-2xl font-bold">
-                Welcome, Laurel Higher Secondary School Team!
-              </CardTitle>
-              <CardDescription className="text-balance text-base">
-                We are thrilled to have you on board. Our platform is designed
-                Manage your teams operations with ease. Stay updated on events,
-                players, members, and more—all in one place. Let’s keep shaping
-                a brighter future together!
-              </CardDescription>
-            </CardHeader>
-          </div>
-          {/* Image Section */}
-          <div className="relative hidden bg-none md:block">
-            <img
-              src={greetingCardImg}
-              alt="standing-infront-of-dashboard"
-              className="absolute h-full w-full object-cover transition-transform duration-500 ease-in-out hover:scale-105"
-            />
-          </div>
-        </CardContent>
-      </Card>
-    </div>
-  );
+	return (
+		<Card className="flex flex-row max-w-full max-h-full items-center justify-center rounded-lg overflow-hidden bg-white shadow-sm">
+			<CardContent className="flex flex-row items-start justify-center gap-3 ">
+				<div className="flex flex-row items-center justify-center gap-3 w-full">
+					<div className="flex flex-col items-start  ">
+						<div className="flex flex-col items-start gap-3 h-full">
+							<h1 className="flex text-4xl font-bold tracking-tight lg:text-5xl">
+								Welcome, Laurel Higher Secondary School Team!
+							</h1>
+							<p className="flex text-xl text-muted-foreground ">
+								We are thrilled to have you on board. Our platform is designed
+								Manage your teams operations with ease. Stay updated on events,
+								players, members, and more—all in one place. Let’s keep shaping
+								a brighter future together!
+							</p>
+						</div>
+					</div>
+					{/* Image Section */}
+					<div className="flex flex-col items-end w-full h-full">
+						<img
+							src={greetingCardImg}
+							alt="standing-infront-of-dashboard"
+							className="w-full h-full object-cover"
+						/>
+					</div>
+				</div>
+			</CardContent>
+		</Card>
+	);
 }
