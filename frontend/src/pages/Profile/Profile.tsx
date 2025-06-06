c9860c15ff5a625b477f2d56a5eb248a779e8fdb
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2025-06-06 18:34:08 +0200

    Refactor Profile component: streamline layout structure, remove unused imports, and enhance styling for improved user experience.

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { UserInfoForm } from "../../components/Form/UserInfoForm";
-import { UserInfoFormDefaultValues } from "../../utils/validators";
-import { Button } from '../../components/common/Button';
 import { FileUploadCard } from '../../components/FileUpload';
 
 
@@ -20,59 +18,25 @@ const Profile: React.FC = () => {
 
 
   return (
-
-
-    <div className="flex flex-col items-start justify-start min-h-screen p-3">
+    <div className="space-y-6 p-6 ">
       <div className="mb-6">
         <h1 >User Profile</h1>
         <h2 >View and Manage all members of your organisation</h2>
       </div>
-      <FileUploadCard description={'Upload a picture for your profile picture'} onFileSelect={function (): void {
-        throw new Error('Function not implemented.');
-      }} />
-      <div className="flex max-w-full mx-auto p-6">
+      <div className="flex gap-6 md:flex-row flex-col">
+        <div className="lg:flex-9/12 md:flex-3/4 flex-1/2">
+          <FileUploadCard description={'Upload a picture for your profile picture'} onFileSelect={function (): void {
+            throw new Error('Function not implemented.');
+          }} />
+        </div>
+        <div className="lg:flex-9/12 md:flex-3/4 flex-1/2">
+          <UserInfoForm onSubmit={handleSubmit} />
+        </div>
 
-        {isSubmitted ? (
-          <div className="p-6">
-            <h2 >Form Submitted Successfully!</h2>
-            <p >Thank you for submitting your information.</p>
-
-            <div className="border-t pt-3 mt-3">
-              <h3 >Submitted Data:</h3>
-              <pre >
-                {JSON.stringify(formData, (key, value) => {
-                  // Format date object for display
-                  if (key === 'dateOfBirth' && value instanceof Date) {
-                    return value.toLocaleDateString();
-                  }
-                  return value;
-                }, 2)}
-              </pre>
-            </div>
-
-            <Button
-              className="btn-base"
-              withIcon={false}
-              onClick={() => {
-                setFormData(null);
-                setIsSubmitted(false);
-              }}
-              variant='primary'
-            >
-              Submit Another Form
-            </Button>
-          </div>
-        ) : (
-          <UserInfoForm
-            defaultValues={UserInfoFormDefaultValues}
-            onSubmit={handleSubmit}
-            formTitle="User Information"
-
-          />
-        )}
       </div>
-    </div>
 
+
+    </div>
   );
 };
 
